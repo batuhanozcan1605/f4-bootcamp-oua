1fcd2daff381bdaf4984617909d9db5f81ce7ade
Batuhan\batuhanozcan1605 <batuhanozcan@etu.edu.tr>
2023-06-12 16:37:48 +0300

    firebase auth
    çalışıyor

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bootcamp_oua_f4/firebasetest/auth.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const AuthScreen(),
     );
   }
 }
