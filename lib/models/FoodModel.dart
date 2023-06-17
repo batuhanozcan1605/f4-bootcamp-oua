9eca8a6d711241715cb4ee577cfedb356fe42aa2
Batuhan\batuhanozcan1605 <batuhanozcan@etu.edu.tr>
2023-06-17 16:54:19 +0300

    -

--- lib/models/FoodModel.dart
+++ lib/models/FoodModel.dart
@@ -1,15 +1,15 @@
 class Foods {
 
   final int category_id;
-  final String food_name;
-  final String? food_image;
+  final String name;
+  final String? image;
   final String place;
   final int? shelfTime;
 
   Foods(
       {required this.category_id,
-      required this.food_name,
-      this.food_image,
+      required this.name,
+      this.image,
       required this.place,
       this.shelfTime}
       );
