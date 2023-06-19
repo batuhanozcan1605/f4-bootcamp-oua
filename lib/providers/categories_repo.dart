b9559f98e941f24c3fe891205fe6a951a0feb8ca
Batuhan\batuhanozcan1605 <batuhanozcan@etu.edu.tr>
2023-06-19 23:11:02 +0300

    add screen precommit

--- lib/providers/categories_repo.dart
+++ lib/providers/categories_repo.dart
@@ -1,4 +1,9 @@
+import 'package:bootcamp_oua_f4/firebasetest/category_model.dart';
 
 class CategoriesRepo {
+
+  List<Category> categories = [];
+
+
 
 }
--- lib/services/data_service.dart
+++ lib/services/data_service.dart
@@ -1,5 +1,6 @@
 import 'package:bootcamp_oua_f4/firebasetest/category_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class DataService {
 
@@ -7,5 +8,20 @@ class DataService {
     final querySnapshot = await FirebaseFirestore.instance.collection('categories').get();
 
     return querySnapshot.docs.map((e) => Category.fromMap(e.data())).toList();
+
+  }
+
+  static FirebaseStorage storage = FirebaseStorage.instance;
+
+  static Future<List<String>> getImageUrls(String path) async {
+    List<String> imageUrls = [];
+    ListResult result = await storage.ref().child(path).listAll();
+
+    for (Reference ref in result.items) {
+      String url = await ref.getDownloadURL();
+      imageUrls.add(url);
+    }
+
+    return imageUrls;
   }
 }
